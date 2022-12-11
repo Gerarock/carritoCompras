98fe2975a0191dea45c903e31631007b4920754c
gveliz <gerardo.veliz.carreno@gmail.com>
2022-12-11 17:59:21 -0300

    segunda pre entrega carrito de compras

--- index.js
+++ index.js
@@ -1,79 +1,74 @@
-/* constante con arreglo de objeto de productos */
-const productos = [
-    { name: 'polera', value: 10000 },
-    { name: 'jeans', value: 30000 },
-    { name: 'zapatilla', value: 50000 },
-    { name: 'cinturon', value: 20000 },
-    { name: 'gorro', value: 3500 }
-];
-
 /* variables globales */
 let carrito = [];
 let nombreCliente;
+let nuevoProducto = '';
 
-//funcion que inicia la compra de productos
-const comprarProducto = () => {
+/* evento click para ver carrito*/
+const verCarrito = document.getElementById("verCarrito");
+
+/* inicia flujo carrito compras */
+verCarrito.addEventListener("click", () => {
     nombreCliente = prompt('Hola, por favor ingresa tu nombre');
-    while (nombreCliente !== null) {
-        if (nombreCliente !== '') {
-            let seleccion = confirm('¡BIENVENIDO ' + nombreCliente?.toUpperCase() + '!, ¿deseas comprar algún producto?, en caso contrario selecciona CANCELAR para salir');
-
-            if (seleccion === true) {
-                seleccionarProducto();
-            } else {
-                nombreCliente = null;
-                alert('Gracias por visitarnos, hasta pronto!!!');
-            }
+    while (nombreCliente !== null && nombreCliente !== '') {
+        let seleccion = confirm('¡Bienvenido ' + nombreCliente?.toUpperCase() + '!\n\n¿Deseas revisar nuestros productos? \n\n Selecciona "CANCELAR" para salir \n Selecciona "ACEPTAR" para continuar');
+        if (seleccion === true) {
+            const ordenaProductos = confirm('¿Quieres ordenar los productos del más barato al más caro?');
+            ordenaProductos ? ordenaMenorMayor() : ordenaMayorMenor();
         } else {
-            alert('Ingrese un usuario');
-            comprarProducto();
+            nombreCliente = null;
+            alert('Gracias por visitarnos, hasta pronto!!!');
         }
     }
-};
+});
+
+/* funcion que ordena productos de menor a mayor precio */
+const ordenaMenorMayor = () => {
+    productos.sort((a, b) => a.precio - b.precio);
+    listaProductos();
+}
+
+/* funcion que ordena productos de mayor a menor precio */
+const ordenaMayorMenor = () => {
+    productos.sort((a, b) => b.precio - a.precio);
+    listaProductos();
+}
 
 /* funcion seleccionar productos */
-const seleccionarProducto = () => {
-    let todosLosProductos = productos.map((product) => product.name + '  -  $' + product.value + '\n');
-    let seleccionaProducto = prompt('Seleccione un producto de nuestra lista \n\n' + todosLosProductos.join(''), 'ej: polera');
-    do {
+const listaProductos = () => {
+    finCompra = false;
+    carrito = [];
+    const productosOrdenados = productos.map(producto => {
+        return '- ' + producto.nombre + ' con un precio de $' + producto.precio
+    });
+    const seleccionaProducto = prompt('Seleccione un producto de nuestra lista: \n\n' + productosOrdenados.join('\n'), 'ej: polera');
+    if (seleccionaProducto !== null) {
         if (seleccionaProducto !== '') {
             agregaProducto(seleccionaProducto);
         } else {
             alert('Debe ingresar un producto');
-            seleccionarProducto();
+            listaProductos();
         }
-    } while (seleccionaProducto === '');
+    } else {
+        const salirCompra = confirm('¿Desea volver al inicio de su compra?');
+        if (!salirCompra) {
+            listaProductos();
+        }
+    }
 }
 
 /* funcion agregar producto */
 const agregaProducto = (seleccionaProducto) => {
-    let precioProducto = 0;
-    if (seleccionaProducto === 'polera' || seleccionaProducto === 'jeans' || seleccionaProducto === 'zapatilla' || seleccionaProducto === 'cinturon' || seleccionaProducto === 'gorro') {
-        switch (seleccionaProducto) {
-            case 'polera':
-                precioProducto = 10000
-                break;
-            case 'jeans':
-                precioProducto = 30000
-                break;
-            case 'zapatilla':
-                precioProducto = 50000
-                break;
-            case 'cinturon':
-                precioProducto = 20000
-                break;
-            case 'gorro':
-                precioProducto = 3500
-                break;
-            default:
-                alert("El dato ingresado no es correcto");
-                precioProducto = 0;
-                break;
-        }
-        let cantidadProductos = parseInt(prompt("¿Cuantas unidades desea comprar?"));
+    let cantidadProductos = 0;
+    do {
+        cantidadProductos = parseInt(prompt("¿Cuantas unidades desea comprar?"));
         if (!isNaN(cantidadProductos)) {
             if (cantidadProductos !== 0) {
-                carrito.push({ seleccionaProducto, cantidadProductos, precioProducto });
+                const producto = productos.find(producto => producto.nombre.toLowerCase() === seleccionaProducto.toLowerCase());
+                if (producto) {
+                    agregaCompra(producto, cantidadProductos);
+                } else {
+                    alert('No contamos con el producto seleccionado');
+                }
             } else {
                 alert('Debes ingresar una cantidad mayor a cero');
                 agregaProducto(seleccionaProducto);
@@ -82,28 +77,86 @@ const agregaProducto = (seleccionaProducto) => {
             alert('Debe ingresar unidades a comprar');
             agregaProducto(seleccionaProducto);
         }
-    } else {
-        alert('No contamos con el producto seleccionado');
-        seleccionarProducto();
-    }
-
-    let nuevaCompra = confirm('¿Desea realizar otra compra?');
-    do {
-        if (nuevaCompra === false) {
-            alert('Selecciona ACEPTAR para ver tus productos comprados');
-            carrito.forEach(compraFinal => {
-                alert(`${compraFinal.cantidadProductos} ${compraFinal.seleccionaProducto}, por un TOTAL de $${compraFinal.cantidadProductos * compraFinal.precioProducto} (valor unitario por producto: $${compraFinal.precioProducto})`);
-            });
-            const totalCompra = carrito.reduce((acc, el) => acc + el.precioProducto * el.cantidadProductos, 0);
-            confirm('El total de tu compra es: $' + totalCompra);
-            const cuotas = calculaCuotas();
-            const intereses = calculaIntereses(cuotas);
-            totalCompraFinal(totalCompra, cuotas, intereses);
-            alert(nombreCliente.toUpperCase() + ' muchas gracias por tu compra, te redirigiremos al inicio de la tienda por si deseas seguir comprando.');
-        } else {
-            seleccionarProducto();
+        if (finCompra === false) {
+            nuevoProducto = confirm('¿Desea agregar otro producto?');
+            if (nuevoProducto) { listaProductos(); }
         }
-    } while (nuevaCompra !== false);
+    } while (nuevoProducto);
+    confirmaCompra();
+}
+
+/* funcion que agrega compra al carrito */
+agregaCompra = (objetoProducto, cantidadProducto) => {
+    const productosRepetidos = carrito.find(producto => producto.id === objetoProducto.id);
+    if (productosRepetidos) {
+        productosRepetidos.cantidad += cantidadProducto;
+    } else {
+        objetoProducto.cantidad += cantidadProducto;
+        carrito.push(objetoProducto);
+    }
+};
+
+/* funcion que confirma la compra o elimina un producto del carrito */
+confirmaCompra = () => {
+    if (finCompra === false) {
+        const listaProductosMap = carrito.map(producto => {
+            return `${producto.nombre} | cantidad: ${producto.cantidad}`;
+        });
+        const confirmar = confirm(`Tu compra a realizar es: \n\n${listaProductosMap.join('\n')} \n\n Selecciona "Cancelar" para eliminar algún producto del carrito \n Selecciona "Aceptar" para continuar con tu compra`);
+
+        if (confirmar) {
+            finalizarCompra();
+        } else {
+            const productoEliminar = prompt(`Ingrese el nombre del producto que desea eliminar: \n\n ${listaProductosMap.join('\n')} \n`);
+            if (productoEliminar !== null) {
+                const productoEliminarFind = carrito.find(producto => producto.nombre.toLowerCase() === productoEliminar.toLowerCase());
+                if (productoEliminarFind) {
+                    eliminaProducto(productoEliminar);
+                } else {
+                    alert('Producto no existe en su carrito');
+                    confirmaCompra();
+                }
+            } else {
+                alert('Ingrese un producto a eliminar');
+                confirmaCompra();
+            }
+        }
+    }
+}
+
+/* funcion que elimina producto del carrito */
+eliminaProducto = (productoEliminar) => {
+    carrito.forEach((producto, index) => {
+        if (producto.nombre.toLowerCase() === productoEliminar) {
+            if (producto.cantidad > 1) {
+                producto.cantidad--
+            } else
+                carrito.splice(index, 1);
+        }
+    });
+    alert(`Un producto de ${productoEliminar} fue eliminado de su carrito`);
+    confirmaCompra();
+};
+
+/* funcion que finaliza la compra */
+finalizarCompra = () => {
+    const totalProductoUnitario = carrito.map(producto => {
+        return `${producto.nombre} | cantidad: ${producto.cantidad} | precio: $${producto.precio} | total: $${producto.precio * producto.cantidad}`;
+    });
+
+    const totalProductos = carrito.reduce((acc, item) => acc + item.cantidad, 0);
+    const totalCompra = carrito.reduce((acc, item) => acc + item.precio * item.cantidad, 0);
+
+    alert(nombreCliente.toUpperCase() + ' el detalle de tu compra es:'
+        + '\n\n' + totalProductoUnitario.join('\n')
+        + '\n\n Cantidad total de productos: ' + totalProductos
+        + '\n\n El TOTAL final de tu compra es: $' + totalCompra
+    );
+    const cuotas = calculaCuotas();
+    const intereses = calculaIntereses(cuotas);
+    totalCompraFinal(totalCompra, cuotas, intereses);
+    alert(`${nombreCliente.toUpperCase()} muchas gracias por tu compra \n\nSeras redirigido al inicio de la tienda por si deseas seguir comprando.`);
+    finCompra = true;
 }
 
 /* funcion calcula cuotas */
@@ -143,5 +196,5 @@ const calculaIntereses = (cuotas) => {
 const totalCompraFinal = (totalCompra, cuotas, intereses) => {
     totalCompra = (totalCompra + intereses);
     let valorCuota = totalCompra / cuotas;
-    alert(`El total a pagar es $${totalCompra} en ${cuotas} cuotas de $${valorCuota.toFixed(2)}`);
+    alert(`El total a pagar es $${totalCompra.toFixed()} en ${cuotas} cuotas de $${valorCuota.toFixed()}`);
 };
---
+++ products.js
@@ -0,0 +1,32 @@
+const productos = [
+    {
+        id: 1,
+        nombre: 'polera',
+        precio: 10000,
+        cantidad: 0
+    },
+    {
+        id: 2,
+        nombre: 'jeans',
+        precio: 30000,
+        cantidad: 0
+    },
+    {
+        id: 3,
+        nombre: 'zapatilla',
+        precio: 50000,
+        cantidad: 0
+    },
+    {
+        id: 3,
+        nombre: 'cinturon',
+        precio: 20000,
+        cantidad: 0
+    },
+    {
+        id: 4,
+        nombre: 'gorro',
+        precio: 3500,
+        cantidad: 0
+    }
+];
